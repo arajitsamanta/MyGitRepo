8bb4e53d3f0d1e02c82a523b8a497b50be962830
arajitsamanta <arijit24@gmail.com>
2017-04-04 22:11:27 -0500

    merged

--- code-practise/src/main/java/com/arajit/codesample/thread/MyDaemonThreadDemo.java
+++ code-practise/src/main/java/com/arajit/codesample/thread/MyDaemonThreadDemo.java
@@ -11,7 +11,7 @@ public class MyDaemonThreadDemo {
 		public void run() {
 			while (true) {
 				try {
-					Thread.sleep(1);
+					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -23,7 +23,8 @@ public class MyDaemonThreadDemo {
 		Thread thread = new MyDaemonThread();
         thread.start();
         System.out.println(thread.isAlive());
-
+        thread.join();
+        System.out.println(thread.isAlive());
 	}
 
 }
