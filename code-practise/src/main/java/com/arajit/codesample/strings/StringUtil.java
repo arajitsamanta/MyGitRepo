3aca8aa234a4b9a8b7d5fcc232b3bc49db66d4ef
Arajit <arijit24@gmail.com>
2017-09-16 10:30:40 -0500

    ok<

--- code-practise/src/main/java/com/arajit/codesample/strings/StringUtil.java
+++ code-practise/src/main/java/com/arajit/codesample/strings/StringUtil.java
@@ -2,6 +2,7 @@ package com.arajit.codesample.strings;
 
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.StringTokenizer;
 
 public class StringUtil {
 
@@ -145,6 +146,38 @@ public class StringUtil {
 		return false;
 		
 	}
+	
+	public static int findDistanceBetweenWords(String inputBody, String pair1, String pair2) {
+		
+		if (inputBody.isEmpty() || pair1.isEmpty() || pair2.isEmpty()) {
+			return -1;
+		}
+		
+		if (pair1.equals(pair2)) {
+			return 0;
+		}
+		
+		StringTokenizer stringTokenizer = new StringTokenizer(inputBody, " ");
+		int distance = 0, globalDistance = Integer.MAX_VALUE;
+		String token;
+		while (stringTokenizer.hasMoreTokens()) {
+			token = stringTokenizer.nextToken();
+			if (token.equals(pair1)) {
+				distance = 0;
+			} else if (token.equals(pair2)) {
+				globalDistance = Math.min(distance, globalDistance);
+			}
+			distance++;
+		}
+		
+		if (globalDistance == Integer.MAX_VALUE || globalDistance == 0) {
+			return -1;
+		} else {
+			return globalDistance;
+		}
+		
+	}
+
 	
 
 }
